aeb5acf18106c817343a98f20a30d9ba0b68fc39
matiasmillain <mem_and1@hotmail.com>
2021-12-14 22:43:28 -0300

    update course.go

--- entity/course.go
+++ entity/course.go
@@ -5,5 +5,6 @@ type Course struct {
 	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
 	Title       string `gorm:"type:varchar(255)" json:"title"`
 	Description string `gorm:"type:text" json:"description"`
-	UserOwner   uint64 `gorm:"not null" json:"-"`
+	UserID      uint64 `gorm:"not null" json:"-"`
+	User        User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
 }
